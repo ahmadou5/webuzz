d5c42793c39b09d4ffd63077363770d4ac281c46
ahmadou5 <ahmadlasauwal@gmail.com>
2024-03-19 11:32:38 +0100

    upnew888

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-navBar flex-col items-center ">
+    <main className="flex min-h-screen bg-black flex-col items-center ">
       <Navbar2 />
       <Home2 />
      
